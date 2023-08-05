c547b98ba6916659930db6901e3bc803e791c24c
Tajuddin Shaik <tajuddin.shaik239@gmail.com>
2023-08-05 12:40:26 +0530

    initial commit

---
+++ Switchprgms.java
@@ -0,0 +1,37 @@
+package core_basics;
+import java.util.*;
+
+public class Switchprgms {
+public static void main(String args[]) {
+//String month;
+System.out.println("please enter month" );
+Scanner sc=new Scanner(System.in);
+int month=sc.nextInt();
+
+switch(month){
+case 1: System.out.println("its a junary");
+break;
+case 2: System.out.println("its a feb");
+break;
+case 3: System.out.println("its a mar");
+break;
+case 4: System.out.println("its a apr");
+break;
+case 5: System.out.println("its a may");
+break;
+case 6: System.out.println("its a jun");
+break;
+default:System.out.println("not a vaild month");
+
+
+
+
+
+
+
+
+
+}
+
+}
+}
