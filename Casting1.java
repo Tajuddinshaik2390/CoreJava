444e19f76334b48533059b48bde50aef7772e0e2
Tajuddin Shaik <tajuddin.shaik239@gmail.com>
2023-08-14 23:53:47 +0530

    initial commit

---
+++ Casting1.java
@@ -0,0 +1,16 @@
+package core_basics;
+
+public class Casting1 {
+
+	public static void main(String[] args) {
+		int number=10;
+	double Double=number;//implicit
+	int number2=(int)Double;//explicit
+
+	
+	System.out.println("value of number is "+number);
+	System.out.println("value of double is "+Double);
+	System.out.println("value of number2 is "+number2);
+	}
+
+}
